a36be1603660f679537cb8b21c9be6b0b1322097
Hanna <hreed3@knights.ucf.edu>
2021-06-14 15:12:48 -0400

    Finished initial implementation

--- DataGenerator.py
+++ DataGenerator.py
@@ -5,27 +5,8 @@ Created on Mon Jun 14 14:46:20 2021
 @author: hreed
 """
 import numpy as np
+import random as rnd
 
-
-
-# Number of cases for each severity :
-mild = 10
-moderate = 10
-high = 10
-
-# Number of features:
-n=5
-
-# Probability for feature bases on severity:
-
-
-
-# We generate a matrix for each severity type:
-    # Each row is an individual
-    # Each column specifies if the feature is present or not (binary)
-mild_cases = np.zeros((mild, n))
-moderate_cases = np.zeros((moderate, n))
-high_cases = np.zeros((high, n))
 
 
 def generate_data(cases, col, prob):
@@ -49,4 +30,46 @@ def generate_data(cases, col, prob):
     # we traverse through the matrix columns
     for i in range(len(cases[:,0])):
         # Generate a random number between 0 and 1
-        
+        rand = rnd.random()
+        if rand <= prob:
+            cases[i,col] = 1
+        else:
+            cases[i,col] = 0
+    
+    return cases
+
+
+
+ 
+##########################
+# Example of use 
+##########################    
+    
+# Number of cases for each severity :
+mild = 10
+moderate = 10
+high = 10
+
+# Number of features:
+n=2
+
+# Probability for feature bases on severity:
+test1 = .2
+test2 = .9
+
+# We generate a matrix for each severity type:
+    # Each row is an individual
+    # Each column specifies if the feature is present or not (binary)
+mild_cases = np.zeros((mild, n))
+moderate_cases = np.zeros((moderate, n))
+high_cases = np.zeros((high, n))
+
+
+
+mild_cases = generate_data(mild_cases, 0, test1)
+mild_cases = generate_data(mild_cases, 1, test2)
+
+print(mild_cases)
+
+
+
