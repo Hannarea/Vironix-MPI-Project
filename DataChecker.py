3bfc0ccd425958ad412db98087450d77001f897f
Hanna <hreed3@knights.ucf.edu>
2021-06-15 14:48:55 -0400

    completed function

--- DataChecker.py
+++ DataChecker.py
@@ -5,31 +5,37 @@ Created on Tue Jun 15 14:20:47 2021
 @author: hreed
 """
 
-# Read in data as matrices
+import numpy as np
 
-# Check data
-
-s = []
-s.append(1)
-print(s)
-
-def check_age(data, lower_bound, upper_bound, col):
+def check_age(data, lower_bound, upper_bound, col, remove = True):
     '''
     Verify that the data in column col is in the given range (lower_bound, upper_bound)
 
     Parameters
     ----------
     data : an np.array matrix where the rows are cases and the columns are features
-    lower_bound : lower bound of feature in column col
-    upper_bound : upper bound of feature in column col
+    lower_bound : lower bound of feature in column col (not strict)
+    upper_bound : upper bound of feature in column col (not strict)
     col : the column we are checking
+    remove: says whether to remove the rows or not 
 
     Returns
     -------
-    The rows that do not pass our check
+    The rows that do not pass our check and the (updated) data
 
     '''
+    problems = []
     
+    for i in range(len(data[:,0])):
+        if data[i, col] <= lower_bound or data[i,col] >= upper_bound:
+            problems.append(i)
+    
+    if remove == True: 
+        for i in range(len(problems)):
+            np.delete(data, problems[i], 0)
+    
+    return problems, data 
+        
     
 
 
